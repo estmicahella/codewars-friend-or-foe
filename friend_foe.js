6d204b00b34074c36a43f0f900ae4d7b675ef5d3
Micahella Estacio <45410954+estmicahella@users.noreply.github.com>
2019-03-10 13:40:41 -0400

    Create friend_foe.js

---
+++ friend_foe.js
@@ -0,0 +1,10 @@
+function friend(friends){
+  friendsLength = [];
+  for(i = 0; i < friends.length; i++){
+  if(friends[i].length === 4){
+  friendsLength.push
+  (friends[i]);
+  }
+  }
+  return friendsLength;
+}
